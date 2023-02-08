060dc14393cebe90360099f03cd2a3255335f487
Jorge Acedo <jorgerag8@hotmail.com>
2023-02-08 15:15:36 -0800

    Replication code done and initializing croos validation code.

---
+++ cross_validation.R
@@ -0,0 +1,12 @@
+" Replication Project COMPAS Analysis
+Cross Validation - Out of Sample Predictions
+Jorge Acedo
+POLI 271 - Winter 2023
+Replication source : https://github.com/propublica/compas-analysis
+"
+
+# Import libraries
+library(dplyr)
+
+# Run replication code to get models
+source("replication.R")
